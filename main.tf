12176247a658a6958d543fb60fc1568bf53d72e9
Richard Michael Coo <richard.michael.coo@gmail.com>
2025-03-16 06:14:32 +0800

    Output keys to replace temporary root credentials

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 module "iam" {
   source = "./aws/iam"
 }
+
+output "terraform_user_access_key" {
+  value     = module.iam.terraform_user_access_key
+  sensitive = true
+}
+
+output "terraform_user_access_key_secret" {
+  value     = module.iam.terraform_user_access_key_secret
+  sensitive = true
+}
