f09364c76f0317a35bb4ce271117a1aabf0cf5ab
Richard Michael Coo <richard.michael.coo@gmail.com>
2025-03-16 06:04:37 +0800

    Add an admin but non-root IAM user

---
+++ aws/iam/terraform-user.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.11.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+resource "aws_iam_user" "terraform_user" {
+  name = "terraform-user"
+}
+
+resource "aws_iam_access_key" "terraform_user_access_key" {
+  user = aws_iam_user.terraform_user.name
+}
+
+resource "aws_iam_user_policy_attachment" "admin_access" {
+  user       = aws_iam_user.terraform_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "terraform_user_access_key" {
+  value     = aws_iam_access_key.terraform_user_access_key.id
+  sensitive = true
+}
+
+output "terraform_user_access_key_secret" {
+  value     = aws_iam_access_key.terraform_user_access_key.secret
+  sensitive = true
+}
